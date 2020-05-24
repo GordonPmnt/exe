05d7f861dcf9adf63613000020f2fdd6cc167ce3
Gordon Preumont <preumontgr@gmail.com>
2020-05-24 18:19:10 +0200

    background hero video

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import colors from './styles/colors';
 
 const Landing = () => {
     const Hero = styled.div`
@@ -7,17 +8,45 @@ const Landing = () => {
         height: 100vh;
     `
 
+    const Background = styled.video`
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        min-width: 100%;
+        min-height: 100%;
+        width: auto;
+        height: auto;
+        z-index: -100;
+        -ms-transform: translateX(-50%) translateY(-50%);
+        -moz-transform: translateX(-50%) translateY(-50%);
+        -webkit-transform: translateX(-50%) translateY(-50%);
+        transform: translateX(-50%) translateY(-50%);
+        background: url(${require('../ressources/background.jpeg')}) no-repeat;
+        background-size: cover;
+    `
+
     const Pitch = styled.h1`
         text-align: center;
         margin: 0;
     `
 
-    const Name = styled.em`
-        color: red;
+    const Name = styled.span`
+        color: ${colors.pinkMexican};
     `
 
     return (
         <Hero>
+            <Background
+                playsInline
+                autoPlay
+                muted
+                loop
+            >
+                <source 
+                    src={require("../ressources/background.mov")}
+                    type="video/mp4"
+                />
+            </Background>
             <Pitch>Hello, I'm <Name>Gordon Preumont</Name>.</Pitch>
             <Pitch>I'm a full-stack developer.</Pitch>
         </Hero>
---
+++ src/components/styles/colors.js
@@ -0,0 +1,6 @@
+const colors = {
+    pinkMexican: '#e4007c',
+    turkoise: '#04c2c9',
+}
+
+export default colors;
