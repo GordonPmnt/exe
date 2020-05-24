b1e99431413b4f0449b2d7f3a3d87e8e42d99fa6
Gordon Preumont <preumontgr@gmail.com>
2020-05-24 16:33:24 +0200

    welcome page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
-    <div>
-      <p>Welcome to eXe</p>
-    </div>
+    <>
+      <Welcome />
+    </>
   );
 }
 
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Welcome = () => {
+    return <p>Welcome to eXe</p>
+}
+
+export default Welcome;
