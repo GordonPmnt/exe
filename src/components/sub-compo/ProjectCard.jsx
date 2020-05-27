9c5257eeecee5cd022f1ce44cc130fd4f774021a
Gordon Preumont <preumontgr@gmail.com>
2020-05-27 23:59:55 +0200

    learn more button

--- src/components/sub-compo/ProjectCard.jsx
+++ src/components/sub-compo/ProjectCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import colors from '../../styles/colors';
+import { Collapse } from '@material-ui/core';
 import device from '../../styles/device';
 import SubSkill from './SubSkill';
 
@@ -17,25 +18,62 @@ const Card = styled.div`
         height: 300px;
         width: 390px;
     }
+
+    &:hover {
+        background-image: none;
 `
 
 const Header = styled.h3`
+    text-align: center;
 `
 
 const LinkProject = styled.a`
+    border: 2px solid ${colors.pinkMexican};
+    text-decoration: none;
+    background: transparent;
+    font-size: 1.4rem;
+    text-align: center;
+    padding: 5px 15px;
+    font-family: "Raleway", sans-serif;
+    color: #616161;
+    transition: all 0.5s;
+    cursor: pointer;
+    position: relative;
+    top: 100px;
+    left: 50%;
+    margin-left: -90px;
+
+    &:hover {
+        background-color: ${colors.pinkMexican};
+        border-color: ${colors.pinkMexican};
+        color: #fff;
+    }
+    &:focus {
+        outline: none;
+    }
 `
 
 const Skill = styled(SubSkill)`
 `
 
 const ProjectCard = ({ id, name, mainStacks, stackIcons, picture }) => {
+    const [checked, setChecked] = React.useState(false);
+
     return (
-        <Card picture={picture}>
-            <Header>{name}</Header>
-            {
-                mainStacks.map((subSkill, index) => <Skill subSkill={subSkill} index={index} svg={stackIcons} />)
-            }
-            <LinkProject>LEARN MORE</LinkProject>
+        <Card 
+            picture={picture} 
+            onMouseEnter={() => setChecked(true)} 
+            onMouseLeave={() => setChecked(false)}
+        >
+            <Collapse in={checked} timeout={700} direction="up">
+                <Header>{name}</Header>
+                {
+                    mainStacks.map((subSkill, index) => <Skill key={id} subSkill={subSkill} index={index} svg={stackIcons} />)
+                }
+            </Collapse>
+            <Collapse in={checked} timeout={200} direction="up">
+                <LinkProject>LEARN MORE</LinkProject>
+            </Collapse>
         </Card>
     )
 }
