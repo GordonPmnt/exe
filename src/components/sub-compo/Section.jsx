9527e03089816d382554fc97957e0a7bb7a60d3a
Gordon Preumont <preumontgr@gmail.com>
2020-05-30 10:08:26 +0200

    Section size can now be customized with props

--- src/components/sub-compo/Section.jsx
+++ src/components/sub-compo/Section.jsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 const Section = styled.section`
     background-color: ${({ background }) => background };
+    ${({ width }) => width ? 'width: ' + width + ';' : '' }
+    ${({ height }) => height ? 'height: ' + height + ';' : '' }
 `
 
 export default Section;
