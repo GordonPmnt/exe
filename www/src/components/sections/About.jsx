4c52282e4fe1ce4c213792be0cc8bf26e88184a2
Gordon Preumont <preumontgr@gmail.com>
2020-08-01 15:05:19 +0200

    rewritte about section

--- www/src/components/sections/About.jsx
+++ www/src/components/sections/About.jsx
@@ -6,11 +6,13 @@ import Title from '../sub-compo/Title';
 import Skill from '../sub-compo/Skill';
 import LinkText from '../sub-compo/LinkText';
 import device from '../../styles/device';
+import Slide from '@material-ui/core/Slide';
 
 const Skills = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: space-evenly;
+    align-items: center;
     margin: 0 15%;
     
     @media ${device.laptop} {
@@ -24,6 +26,7 @@ const Me = styled.div`
     align-items: center;
     padding-bottom: 50px;
     margin: 0 10%;
+    min-height: 510px;
 
     @media ${device.laptop} {
         margin: 0 20%;
@@ -75,35 +78,60 @@ const Break = styled.div`
     margin: 10px;
 `
 
+const Swipe = styled.div`
+    cursor: pointer;
+    text-align: center;
+    padding: 40px;
+    transition: all 0.5s;
+    color: ${colors.turkoise};
+
+    &:hover {
+        color: ${colors.pinkMexican};
+    }
+`
+
 const About = () => {
     const skills = require('../../data/skills.json')
+
+    const [showSkills, setShowSkills] = useState(true)
+    const [showMe, setShowMe] = useState(false)
+
+    const handleClick = () => {
+        setShowSkills(!showSkills)
+        setShowMe(!showMe)
+    }
 
     return (
         <Section id="about" background={colors.white}>
             <Title title={"ABOUT"} />
-            <Skills>
-            {
-                skills.map(skill => <Skill key={skill.id} {...skill} />)
-            }
-            </Skills>
-            <Me>
-                <Avatar src={require('../../ressources/pic_profile.jpg')} alt="Gordon Preumont"/>
-                <Bio>
-                    <BioHeader>Who's this guy?</BioHeader>
-                    <BioText>
-                        With a master’s degree in business management, 
-                        I worked 10 years in finance in various positions and areas.
-                        <Break />
-                        Passionate about programming and technologies, I decided to 
-                        change careers to become a <b>Full-Stack Developer</b> to better 
-                        meet my values and live from my passion. 
-                        <Break />
-                        Today I am fulfilled and more motivated than ever. 
-                        <Break />
-                        <Contact href="#contact">Let's do something together !</Contact>
-                    </BioText>
-                </Bio>
-            </Me>
+            <Swipe onClick={handleClick}>{showSkills ? "Who's that guy ?" : "Show skills"}</Swipe>
+            <Slide direction="left" in={showSkills} mountOnEnter unmountOnExit>
+                <Skills>
+                    {
+                        skills.map(skill => <Skill key={skill.id} {...skill} />)
+                    }
+                </Skills>
+            </Slide>
+            <Slide direction="left" in={showMe} mountOnEnter unmountOnExit>
+                <Me>
+                    <Avatar src={require('../../ressources/pic_profile.jpg')} alt="Gordon Preumont"/>
+                    <Bio>
+                        <BioHeader>Who's this guy?</BioHeader>
+                        <BioText>
+                            With a master’s degree in business management, 
+                            I worked 10 years in finance in various positions and areas.
+                            <Break />
+                            Passionate about programming and technologies, I decided to 
+                            change careers to become a <b>Full-Stack Developer</b> to better 
+                            meet my values and live from my passion. 
+                            <Break />
+                            Today I am fulfilled and more motivated than ever. 
+                            <Break />
+                            <Contact href="#contact">Let's do something together !</Contact>
+                        </BioText>
+                    </Bio>
+                </Me>
+            </Slide>
         </Section>
     )
 }
